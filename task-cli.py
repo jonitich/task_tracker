f8f2d2756394dee6efe65817b6333b6ff408cd28
Jonatan Redondo <jredondo@theworkshop.com>
2024-12-30 17:56:08 +0100

    first commit

--- task-cli.py
+++ task-cli.py
@@ -1,6 +1,5 @@
 import time
 import os
-import io
 from pathlib import Path
 import json
 import argparse
@@ -16,7 +15,7 @@ class Task():
     
     def __init__(self, task):
         self.description = task
-        self.state = "todo"
+        self.state = "to-do"
         self.creation_at = time.ctime()
         self.update_at = time.ctime()
     
@@ -44,25 +43,42 @@ class Task():
         }
         tasks.append(task)
         self.save_tasks(tasks)
+        print(f"Task {new_id} created successfully.")
     
-    def mark_task(self, id):
+    def mark_task(self, id, state):
         """ Mark a task as: In progress, Todo or Done. """
-        
+        pass
 
-def handle_task(args):
+
+
+
+def handle_add_task(args):
     task = Task(args.task)
     task.create_task()
 
+def handle_mark_tasks(args):
+    tasks = Task.load_tasks()
+    print(tasks)
+    print(type(tasks))
 
 def main():
     parser =  argparse.ArgumentParser(prog="task-cli")
     subparser = parser.add_subparsers(help='subcommand help')
     
-    # Add subcommand
+    # Add Subcommand
     parser_add = subparser.add_parser('add', help="add a task to the list")
     parser_add.add_argument('task', help="Name of the task")
+    parser_add.set_defaults(func=handle_add_task)
+    
+    # Mark Subcommand
+    parser_mark = subparser.add_parser('mark', help="mark a task as: 'to-do, in-progress or done.'")
+    parser_mark.add_argument('id', help="ID of the task to mark.", type=int)
+    parser_mark.add_argument("state", choices=["to-do", "in-progress", "done"], type=str)
+    parser_mark.set_defaults(func=handle_mark_tasks)
+    
+    # Parse Args
     args = parser.parse_args()
-    handle_task(args)
+    args.func(args)
 
 if __name__ == "__main__":    
     main()
